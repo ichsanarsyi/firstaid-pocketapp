7482e1bb7df24de8d5212c45dba7553dced5e45b
DwiNurfatma <dwinurfatma10f@gmail.com>
2021-10-22 22:25:34 +0700

    kalkulator

--- src/Components/Kalkulator.js
+++ src/Components/Kalkulator.js
@@ -1,19 +1,175 @@
-import React, { Component } from "react";
-import axios from "axios";
-import Grid from "@material-ui/core/Grid";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
+import React, { Fragment } from "react";
 import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import bg from "./bg (2).png";
 import CardMedia from "@material-ui/core/CardMedia";
-import { Modal } from "antd";
-import "antd/dist/antd.css";
+class Kalkulator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChangeStuff = this.handleChangeStuff.bind(this);
+    this.state = {
+      stuff: [
+        ["Kapas", 5000],
+        ["Kasa", 7000],
+        ["Plester", 5000],
+        ["Handsaplast", 500],
+        ["Kantung Plastik", 6000],
+        ["Senter", 25000],
+        ["Kain Perban", 50000],
+        ["Pita Merah", 8000],
+        ["Tambang", 8000],
+      ],
+      obat: [
+        ["Betadine", 17000],
+        ["Minyak Kayu Putih", 13000],
+        ["Balsem", 7000],
+        ["Paracetamol", 4000],
+        ["Minyak Tawon", 27000],
+        ["CTM", 10000],
+        ["Milanta", 5000],
+        ["Diapet", 2000],
+        ["Dexametason", 15000],
+        ["Dettol Antiseptik", 14000],
+        ["Dettol Handsinitizer", 10000],
+      ],
 
-export default class Kalkulator extends Component {
-	render() {
-		return (
-			<></>
-		)
-	}
+      menu: {
+        menu1: 0,
+        menu2: 0,
+        menu3: 0,
+      },
+      totalTagihan: 0,
+    };
+  }
+
+  handleCalculation = () => {
+    const { menu1, menu2 } = this.state.menu;
+    var total = menu1 + menu2;
+    this.setState({
+      ...this.state.menu,
+      totalTagihan: total,
+    });
+  };
+
+  handleChangeStuff(e) {
+    e.preventDefault();
+    const { menu } = this.state;
+    const { name } = e.target;
+    var index = e.nativeEvent.target.selectedIndex;
+    const { value } = e.nativeEvent.target[index];
+    this.setState(
+      {
+        menu: {
+          ...menu,
+          [name]: Number(value),
+        },
+      },
+      this.handleCalculation
+    );
+  }
+
+  render() {
+    const { obat, stuff, totalTagihan } = this.state;
+    return (
+      <>
+        <div>
+          <div style={{ backgroundColor: "#360400" }}>
+            <AppBar
+              style={{
+                padding: "10px",
+                marginBottom: "100px",
+                backgroundColor: "#c70d00",
+              }}
+            >
+              <Typography
+                style={{ margin: "auto", color: "#ededed", fontWeight: "bold" }}
+              >
+                Kasir
+              </Typography>
+            </AppBar>
+
+            <div style={{ height: "75%" }}>
+              <div className="cashier-calculator">
+                <div>
+                  <h2>Daftar Pesanan</h2>
+                  <br />
+                  <select
+                    onChange={this.handleChangeStuff}
+                    name="menu1"
+                    style={{
+                      cursor: "pointer",
+                      margin: "20px",
+                      padding: "10px",
+                      width: "200px",
+                      backgroundColor: "#c70d00",
+                      color: "white",
+                    }}
+                  >
+                    <option value="0">Peralatan</option>
+                    <Fragment>
+                      {stuff.map((alat) => {
+                        return <option value={alat[1]}>{alat[0]}</option>;
+                      })}
+                    </Fragment>
+                  </select>
+                  <br />
+                  <select
+                    onChange={this.handleChangeStuff}
+                    name="menu2"
+                    style={{
+                      cursor: "pointer",
+                      margin: "20px",
+                      padding: "10px",
+                      width: "200px",
+                      backgroundColor: "#c70d00",
+                      color: "white",
+                    }}
+                  >
+                    <option value="0">Obat obatan</option>
+                    <Fragment>
+                      {obat.map((obat) => {
+                        return <option value={obat[1]}>{obat[0]}</option>;
+                      })}
+                    </Fragment>
+                  </select>
+                  <br />
+
+                  <h5 style={{ color: "white", margin: "20px" }}>
+                    Total Belanjaan: Rp {totalTagihan}{" "}
+                  </h5>
+                </div>
+                <h2
+                  style={{
+                    color: "white",
+                    textAlign: "center",
+                    flex: "1 0 100%",
+                    margin: "auto",
+                  }}
+                >
+                  Silakan Bayar: Rp {totalTagihan}
+                </h2>
+              </div>
+            </div>
+            <CardMedia
+              style={{
+                marginLeft: "1000px",
+                alignItems: "left",
+
+                marginTop: "20px",
+                marginBottom: "50px",
+
+                borderRadius: "8px",
+                width: "400px",
+                height: "300px",
+              }}
+              component="img"
+              image={bg}
+            />
+          </div>
+        </div>
+      </>
+    );
+  }
 }
+
+export default Kalkulator;
